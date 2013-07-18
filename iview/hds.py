43c5a0e80985bbb23b87420f460f44894941eed7
Martin Panter <vadmium  gmailcom>
2013-07-18 14:58:28 +1000

    Parsing manifest and bootstrap info to get fragment numbering
    This is based on looking at KSV’s AdobeHDS.php script

--- iview/hds.py
+++ iview/hds.py
@@ -15,6 +15,7 @@ import http.client
 from sys import stderr
 from urllib.error import HTTPError
 from urllib.parse import urljoin, urlsplit
+from io import BytesIO
 
 def fetch(*pos, dest_file, quiet=False, frontend=None, abort=None, **kw):
     url = manifest_url(*pos, **kw)
@@ -27,16 +28,128 @@ def fetch(*pos, dest_file, quiet=False, frontend=None, abort=None, **kw):
         with session.open(url) as response:
             manifest = ElementTree.parse(response)
         
-        # TODO: this should be determined from bootstrap info presumably
-        (frags, _) = manifest.findtext(F4M_NAMESPACE + "duration").split(".")
-        frags = (int(frags) + 2) // 3
-        
-        # TODO: determine preferred bitrate, max bitrate, etc
-        media = manifest.find(F4M_NAMESPACE + "media")  # Just pick the first one!
+        url = manifest.findtext(F4M_NAMESPACE + "baseURL", url)
+        manifest.findtext(F4M_NAMESPACE + "duration")
         
         player = player_verification(manifest)
         if not quiet:
             print(player, file=stderr)
+        
+        # TODO: determine preferred bitrate, max bitrate, etc
+        media = manifest.find(F4M_NAMESPACE + "media")  # Just pick the first one!
+        
+        href = media.get("href")
+        if href is not None:
+            href = urljoin(url, href)
+            bitrate = media.get("bitrate")  # Save this in case the child manifest does not specify a bitrate
+            raise NotImplementedError("/manifest/media/@href -> child manifest")
+        bitrate = media.get("bitrate")  # Not necessarily specified
+        
+        bsid = media.get("bootstrapInfoId")
+        for bootstrap in manifest.findall(F4M_NAMESPACE + "bootstrapInfo"):
+            if bsid is None or bsid == bootstrap.get("id"):
+                break
+        else:
+            msg = "/manifest/bootstrapInfoId[@id={!r}]".format(bsid)
+            raise LookupError(msg)
+        
+        bsurl = bootstrap.get("bootstrapUrl")
+        if bsurl is not None:
+            bsurl = urljoin(urljoin(url, bsurl), "?" + player)
+            msg = "manifest/bootstrapInfo/@bootstrapUrl"
+            raise NotImplementedError(msg)
+        else:
+            bootstrap = bootstrap.text
+            bootstrap = b64decode(bootstrap.encode("ascii"), validate=True)
+            bootstrap = BytesIO(bootstrap)
+        
+        (type, _) = read_box_header(bootstrap)
+        assert type == b"abst"
+        streamcopy(bootstrap, nullwriter, 1 + 3 + 4)
+        byte = read_int(bootstrap, 1)
+        live = bool(byte & 0x20)
+        if not byte & 0x10:
+            # start fresh seg, frag tables?
+            pass
+        streamcopy(bootstrap, nullwriter, 4 + 8 + 8)
+        read_string(bootstrap)
+        count = read_int(bootstrap, 1)
+        for _ in range(count):
+            read_string(bootstrap)
+        count = read_int(bootstrap, 1)
+        for _ in range(count):
+            read_string(bootstrap)
+        read_string(bootstrap)
+        read_string(bootstrap)
+        
+        assert read_int(bootstrap, 1) == 1
+        (type, size) = read_box_header(bootstrap)
+        assert type == b"asrt"
+        seg_runs = list()
+        streamcopy(bootstrap, nullwriter, 1 + 3)
+        count = read_int(bootstrap, 1)
+        size -= 1 + 3 + 1
+        for _ in range(count):
+            size -= len(read_string(bootstrap))
+        count = read_int(bootstrap, 4)
+        size -= 4
+        for _ in range(count):
+            run = dict()
+            run["first"] = read_int(bootstrap, 4)
+            run["frags"] = read_int(bootstrap, 4) & 0x7FFFFFFF
+            size -= 8
+            seg_runs.append(run)
+        assert not size
+        
+        assert read_int(bootstrap, 1) == 1
+        (type, size) = read_box_header(bootstrap)
+        assert type == b"afrt"
+        frag_runs = list()
+        streamcopy(bootstrap, nullwriter, 1 + 3 + 4)
+        count = read_int(bootstrap, 1)
+        size -= 1 + 3 + 4 + 1
+        for _ in range(count):
+            size -= len(read_string(bootstrap))
+        count = read_int(bootstrap, 4)
+        size -= 4
+        for _ in range(count):
+            run = dict()
+            run["first"] = read_int(bootstrap, 4)
+            run["timestamp"] = read_int(bootstrap, 8)  # (ms?)
+            run["duration"] = read_int(bootstrap, 4)
+            size -= 16
+            if not run["duration"]:
+                run["discontinuity"] = read_int(bootstrap, 1)
+                size -= 1
+            frag_runs.append(run)
+        assert not size
+        
+        last_frag_run = frag_runs[-1]
+        if not last_frag_run.get("discontinuity", True):
+            live = False
+            last_frag_run = frag_runs[-2]
+        
+        frags = seg_runs[-1]["frags"]
+        for (i, run) in enumerate(seg_runs[:-1]):
+            frags += (seg_runs[i + 1]["first"] - run["first"]) * run["frags"]
+        
+        invalid_count = not frags
+        if not invalid_count:
+            frags += frag_runs[0]["first"] - 1
+        frags = max(frags, last_frag_run["first"])
+        
+        if live:
+            start_seg = seg_runs[-1]["first"]
+        else:
+            start_seg = seg_runs[0]["first"]
+        
+        if live and not invalid_count:
+            start_frag = frags - 2
+        else:
+            start_frag = frag_runs[0]["first"] - 1
+        start_frag = max(start_frag, 0)
+        assert start_frag < frags
+        
         media_url = media.get("url")
         metadata = media.findtext(F4M_NAMESPACE + "metadata")
         metadata = b64decode(metadata.encode("ascii"), validate=True)
@@ -47,19 +160,12 @@ def fetch(*pos, dest_file, quiet=False, frontend=None, abort=None, **kw):
             flv.write(metadata)
             flv.write(bytes.fromhex("00019209"))
             
-            for frag in range(frags):
+            for frag in range(start_frag, frags):
                 frag += 1
-                frag_url = "{}Seg1-Frag{}?{}".format(media_url, frag, player)
+                frag_url = "{}Seg{}-Frag{}?{}".format(
+                    media_url, start_seg, frag, player)
                 frag_url = urljoin(url, frag_url)
-                
-                try:
-                    response = session.open(frag_url)
-                except HTTPError as err:
-                    if err.code != http.client.NOT_FOUND:
-                        raise
-                    # Relying on requests for fragments after the final
-                    # fragment to return 404 "Not found" errors
-                    break
+                response = session.open(frag_url)
                 
                 while True:
                     (boxtype, boxsize) = read_box_header(response)
@@ -88,11 +194,6 @@ def fetch(*pos, dest_file, quiet=False, frontend=None, abort=None, **kw):
                 else:
                     stderr.write("\rFrag {}; {:.1F} MB\r".format(frag, size / 1e6))
                     stderr.flush()
-            else:
-                if not frontend:
-                    print(file=stderr)
-                msg = "Fragment number would exceeded {}".format(frags)
-                raise NotImplementedError(msg)
             if not frontend:
                 print(file=stderr)
 
@@ -133,6 +234,15 @@ def read_int(stream, size):
     bytes = stream.read(size)
     assert len(bytes) == size
     return int.from_bytes(bytes, "big")
+
+def read_string(stream):
+    buf = bytearray()
+    while True:
+        b = stream.read(1)
+        assert b
+        if not ord(b):
+            return buf
+        buf.extend(b)
 
 class PersistentConnectionHandler(urllib.request.BaseHandler):
     def __init__(self, *pos, **kw):
@@ -256,6 +366,7 @@ class NullWriter(BufferedIOBase):
 nullwriter = NullWriter()
 
 def streamcopy(input, output, length, abort=None):
+    assert length >= 0
     while length:
         if abort and abort.is_set():
             raise SystemExit()
