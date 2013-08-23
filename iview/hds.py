b40b26e46c1e6ce7abb1abeb0a589902c23acfb0
Martin Panter <vadmium  gmailcom>
2013-08-23 03:15:58 +0000

    Parsing the FLV metadata from the manifest file

--- iview/hds.py
+++ iview/hds.py
@@ -15,6 +15,7 @@ from urllib.error import HTTPError
 from urllib.parse import urljoin, urlsplit
 from io import BytesIO
 from .parser import xml_text_elements
+from struct import Struct
 
 def fetch(*pos, dest_file, frontend=None, abort=None, player=None, key=None,
 **kw):
@@ -53,7 +54,11 @@ def fetch(*pos, dest_file, frontend=None, abort=None, player=None, key=None,
             media_url = urljoin(bootstrap["server_base_url"], media_url)
         media_url = urljoin(url, media_url)
         
-        metadata = media["metadata"]
+        metadata = media.get("metadata")
+        if metadata:
+            (name, value) = parse_metadata(metadata)
+            assert name == b"onMetaData"
+        
         with open(dest_file, "wb") as flv:
             flv.write(b"FLV")  # Signature
             flv.write(bytes((1,)))  # File version
@@ -339,6 +344,51 @@ def player_verification(manifest, player, key):
     # The "hdntl" parameter must be passed either in the URL or as a cookie
     return "pvtoken={}&{}".format(
         urlencode_param(pvtoken), urlencode_param(hdntl))
+
+def parse_metadata(data):
+    stream = BytesIO(data)
+    name = parse_scriptdatavalue(stream)
+    value = parse_scriptdatavalue(stream)
+    assert stream.tell() >= len(data)
+    return (name, value)
+
+def parse_scriptdatavalue(stream):
+    type = read_int(stream, 1)
+    return scriptdatavalue_parsers[type](stream)
+
+scriptdatavalue_parsers = dict()
+
+def parse_number(stream):
+    (number,) = DOUBLE_BE.unpack(stream.read(DOUBLE_BE.size))
+    return number
+DOUBLE_BE = Struct(">d")
+scriptdatavalue_parsers[0] = parse_number
+
+def parse_boolean(stream):
+    return bool(read_int(stream, 1))
+scriptdatavalue_parsers[1] = parse_boolean
+
+def parse_string(stream):
+    length = read_int(stream, 2)
+    string = stream.read(length)
+    assert len(string) == length
+    return string
+scriptdatavalue_parsers[2] = parse_string
+
+def parse_ecma_array(stream):
+    streamcopy(stream, nullwriter, 4)  # Approximate length
+    array = dict()
+    while True:
+        name = parse_string(stream)
+        value = parse_scriptdatavalue(stream)
+        if value is StopIteration:
+            return array
+        array[name] = value
+scriptdatavalue_parsers[8] = parse_ecma_array
+
+def parse_end(stream):
+    return StopIteration
+scriptdatavalue_parsers[9] = parse_end
 
 def skip_box(stream, abort=None):
     (_, size) = read_box_header(stream)
